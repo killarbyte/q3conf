da1b601c0e1298a812e97ff750afd26b0c86dbd8
Anton K <killarbyte@gmail.com>
2020-08-09 21:18:27 +0300

    msg

---
+++ tmp/^start.quake3.x64.cmd
@@ -0,0 +1,13 @@
+:: "III Project" MOD pak by "amRa" '2019
+:: http://q3.torrtuga.ru
+
+:: Command line batch file
+quake3e.x64.exe +set sv_pure 0 +set fs_game osp +set ui_cdkeychecked 1 ^
+  +set com_introplayed 1 +set r_inGameVideo 0 +set com_skipidlogo 1 ^
+  +set r_fullscreen 1 +set r_modefullscreen -2 +set r_mode -2 +set r_customwidth 1920 +set r_customheight 1200 +set r_renderscale 3 ^
+  +set vid_xpos 200 +set vid_ypos 200 ^
+  +set com_zonemegs 64 +set com_soundmegs 64 +set com_hunkmegs 512 ^
+  +set r_picmip 6 +set r_texturemode GL_NEAREST ^
+  +set s_volume 0.08 +set s_musicvolume 0 +set s_ambient 0 ^
+::  +connect q3msk.ru:27977
+
